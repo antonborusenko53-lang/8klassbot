37582043ee3d838cc68312b926008e30bf0ec603
antonborusenko53-lang <antonborusenko53@gmail.com>
2025-12-26 17:06:14 +0200

    Change Telegram bot token
    Updated the bot token for the Telegram bot.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 from threading import Thread
 
 # ================== НАЛАШТУВАННЯ ==================
-TOKEN = "8453039217:AAGBzZmUfAoeukx9pfU1X39abYsiKMqCieA"
+TOKEN = "8453039217:AAEZYmcyehIe1flEeLSMdz1G6VucRXLYPLM"
 bot = telebot.TeleBot(TOKEN)
 
 ADMINS = [1013047918, 5245235883]
